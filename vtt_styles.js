952f52cd12e320df0b92c1967666de50a1b8c4a7
j.mir.prazak <j.mir.prazak@gmail.com>
2020-09-24 16:03:13 +0200

    abc

--- vtt_styles.js
+++ vtt_styles.js
@@ -74,7 +74,7 @@ function start() {
 		for (var i = 0; i < tracks.length; i++) {
 			if ( tracks[i].kind == "subtitles" ) {
 				// console.log(tracks[i])
-				var listener = tracks[i].addEventListener('cuechange',  changeColor( track, cue ).bind( null, tracks[i], video_cue ) )
+				var listener = tracks[i].addEventListener('cuechange',  changeColor( tracks[i] ) )
 
 				eventListeners.push(listener)
 			}
