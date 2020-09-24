218cb23d3870a112146d350826d039550d40d37c
j.mir.prazak <j.mir.prazak@gmail.com>
2020-09-24 13:52:56 +0200

    first

---
+++ vtt_styles.js
@@ -0,0 +1,44 @@
+var eventListeners = new Array();
+
+var video = document.getElementsByTagName("video");
+console.log(video)
+
+var tracks = document.getElementsByTagName("track");
+
+for (var i = 0; i < tracks.length; i++) {
+	if ( tracks[i].kind == "subtitles" ) {
+		// console.log(tracks[i])
+		var listener = tracks[i].addEventListener('cuechange',  function(track){
+			// console.log(track.track)
+			var  track = track;
+
+			if ( track.track.activeCues ) {
+
+				for (var j = 0; j < track.track.activeCues.length; j++) {
+
+					console.log(track.track.activeCues[j].id)
+
+					if ( track.track.activeCues[j].id != "" ) {
+
+						document.styleSheets[0].rules[document.styleSheets[0].rules.length-1].valueOf("video::cue").style.color = track.track.activeCues[j].id;
+
+					}
+
+					else {
+
+						document.styleSheets[0].rules[document.styleSheets[0].rules.length-1].valueOf("video::cue").style.color = "white";
+
+					}
+				}
+			}
+
+
+		}.bind( null, tracks[i]) )
+
+		eventListeners.push(listener)
+	}
+
+}
+}
+
+setTimeout(start, 3000)
